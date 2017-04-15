a4f1fa991f3d6b99d71a08bd3eb66fea8e9e79b4
aalvare3 <aalvare3@stevens.edu>
2017-04-14 23:27:11 -0400

    Updated Module

--- UpDownCounter/UDCounterModule.vhd
+++ UpDownCounter/UDCounterModule.vhd
@@ -30,12 +30,44 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity UDCounterModule is
+  port(Cin,Clk,PE,Res,UD: in std_logic;
+       P: in std_logic_vector(1 to 4);
+       Cout: out std_logic;
+       Q: out std_logic_vector(1 to 4));
 end UDCounterModule;
 
 architecture Behavioral of UDCounterModule is
 
+
+
 begin
-
-
+  
+  SetCout: process(Q,UD,Cin) is
+  begin
+    if Cin = '1' then
+      Cout <= '1';
+    elsif (UD = '1' and Q(1) = '1' and Q(4) = '1') then
+      Cout <= '0';
+    elsif (UD = '0' and Q = "0000") then
+      Cout <= '0';
+    else
+      Cout = '1';
+    end if;
+  end process;
+      
+  SetQ: process(Cin,PE,Res,Clk) is
+    
+  begin
+    if Res = '1' then
+      Q <= "0000";
+    elsif PE = '1' then
+      Q <= P;
+    elsif Cin = '1' then
+      null;
+    elsif rising_edge(Clk) = '1' then
+      
+    end if;
+  end process;
+      
 end Behavioral;
 
