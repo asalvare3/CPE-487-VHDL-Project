b1cb0d982b93e0ac4aae44b167ccc322ab81d22b
aalvare3 <aalvare3@stevens.edu>
2017-04-23 13:42:55 -0400

    Updated File

--- UpDownCounter/UDCounterModule.vhd
+++ UpDownCounter/UDCounterModule.vhd
@@ -19,7 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_unsigned.all; -- cheat
 
 
 -- Uncomment the following library declaration if using
